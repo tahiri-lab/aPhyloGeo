58a797a23dfaa26826d8b5e4140d75cf7a4db81a
my-linh-luu <mylinh_93@hotmail.com>
2021-05-21 16:32:55 -0400

    changed the script to delete the sequences.fasta file everytime

--- fetch_data.sh
+++ fetch_data.sh
@@ -3,5 +3,14 @@
 # This script retrieves all the fasta files in the data repository and create a new fasta file named 'sequences.fasta' containing all the sequences.
 # This file will then serves as an input in the muscle executable.
 
-find data -name *.fasta -exec cat {} >> sequences.fasta \;
+# Set the filename
+filename='sequences.fasta'
+
+# Check if the file already exists, if it does, delete it
+if [ -f $filename ]; then
+    rm sequences.fasta
+fi
+
+# Create a new sequences.fasta file containing all the sequences in the data repo
+find data -name "*.fasta" -exec cat {} >> sequences.fasta \;
 
