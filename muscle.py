3fc29fe9b7ef9211b433c1dc52d06d21d8d0bbce
my-linh-luu <mylinh_93@hotmail.com>
2021-06-01 22:03:09 -0400

    added function to change name of sequences

--- muscle.py
+++ muscle.py
@@ -1,7 +1,28 @@
 import subprocess
 
-def fetchingData():
-    subprocess.call("./fetch_data.sh")
+def fetchingSequences():
+    subprocess.run("./fetch_data.sh")
+
+def changeNameSequences():
+    sequences_file = open("sequences.fasta", "r")
+    list_of_lines = sequences_file.readlines()
+    for index in range(len(list_of_lines)):
+        if list_of_lines[index].startswith(">"):
+            splitted_line = list_of_lines[index].split("/")
+            name = ">" + splitted_line[2] + "\n"
+            list_of_lines[index] = name
+    
+    sequences_file = open("sequences.fasta", "w")
+    sequences_file.writelines(list_of_lines)
+    sequences_file.close()
+
+
+def alignSequences():
+    subprocess.run(["./muscle", "-in", "sequences.fasta", "-phyiout", "infile", "-maxiters", "1", "-diags"])
+
+# def createBoostrap
 
 if __name__ == '__main__':
-    fetchingData()
+    fetchingSequences()
+    changeNameSequences()
+    alignSequences()
