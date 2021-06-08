eaebd73089e6c1dabc03cdcee8a3858a9b2d788d
my-linh-luu <mylinh_93@hotmail.com>
2021-06-08 15:03:50 -0400

    redid the pipeline without botswana sequence

--- fetch_data.sh
+++ fetch_data.sh
@@ -4,11 +4,11 @@
 # This file will then serves as an input in the muscle executable.
 
 # Set the filename
-filename='sequences.fasta'
+filename='output/sequences.fasta'
 
 # Check if the file already exists, if it does, delete it
 if [ -f $filename ]; then
-    rm sequences.fasta
+    rm output/sequences.fasta
 fi
 
 # Create a new sequences.fasta file containing all the sequences in the data repo
