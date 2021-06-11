64a8cc40e42387507449921e5da8a6af771b65c6
my-linh-luu <mylinh_93@hotmail.com>
2021-06-11 13:59:01 -0400

    get N gene

--- test.py
+++ test.py
@@ -226,7 +226,25 @@ def getORF8Gene():
     new_file.close()
 
 
+def getNGene():
+    sequences_file = open("output/reference/sequences.fasta", "r").read()
+    list_of_sequences = sequences_file.split(">")
+    # ici, presence de mutation dans les 10 premieres bases, d'ou la necessite de mettre des inconnus
+    s = 'ATGTCT[CG][AT][TA]AAT(.*)TCAGGCCTAA'
+    new_file = open("output/N_gene/N_gene.fasta", "w")
+    for index in range(len(list_of_sequences)):
+        if list_of_sequences[index] == "":
+            continue
+        name = list_of_sequences[index].split("\n")[0]
+        gene_sequence = list_of_sequences[index].replace("\n", "")
+        gene_sequence = (re.search(s, gene_sequence).group())
+        new_file.writelines(">" + name + "\n")
+        new_file.writelines(gene_sequence + "\n")
+
+    new_file.close()
+
+
 if __name__ == '__main__':
 #    fetchingSequences()
 #    changeNameSequences()
-    getORF8Gene()
+    getNGene()
