9938bad872c7e3e03a536abaab30ccd469ba122b
my-linh-luu <mylinh_93@hotmail.com>
2021-06-15 15:41:22 -0400

    added the function to retrieve ORF1ab gene

--- ORF1abgene.py
+++ ORF1abgene.py
@@ -3,7 +3,7 @@ from pipeline import *
 
 if __name__ == '__main__':
     gene = 'ORF1ab'
-    getGene('ORF1ab', 'ATGGAGAGCC(.*)TAACAACTAA')
+    getGene(gene, 'ATGGAGAGCC(.*)TAACAACTAA')
     alignSequences(gene)
     createBoostrap(gene)
     createDistanceMatrix(gene)
