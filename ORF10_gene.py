7d358fa15393279522a7956b4366a4126c214b0a
my-linh-luu <mylinh_93@hotmail.com>
2021-06-16 09:20:57 -0400

    script to get ORF10 gene

---
+++ ORF10_gene.py
@@ -0,0 +1,11 @@
+from gene import *
+from pipeline import *
+
+if __name__ == '__main__':
+    gene = 'ORF10'
+    getGene(gene, 'ATGGGCTATA(.*)TCTCACATAG')
+    alignSequences(gene)
+    createBoostrap(gene)
+    createDistanceMatrix(gene)
+    createUnrootedTree(gene)
+    createConsensusTree(gene)
