dbac896063dbef6c6f6bc9c0b96940195bc8331e
my-linh-luu <mylinh_93@hotmail.com>
2021-06-15 20:10:09 -0400

    added script for ORF6 gene

---
+++ ORF6_gene.py
@@ -0,0 +1,11 @@
+from gene import *
+from pipeline import *
+
+if __name__ == '__main__':
+    gene = 'ORF6'
+    getGene(gene, 'ATGTTTCATC(.*)GATTGA[CT]TAA')
+    alignSequences(gene)
+    createBoostrap(gene)
+    createDistanceMatrix(gene)
+    createUnrootedTree(gene)
+    createConsensusTree(gene)
