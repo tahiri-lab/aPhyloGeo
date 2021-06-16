86f95d8e3f409d305fab4009102efe5e22387edc
my-linh-luu <mylinh_93@hotmail.com>
2021-06-16 08:41:48 -0400

    script to get M gene

---
+++ M_gene.py
@@ -0,0 +1,11 @@
+from gene import *
+from pipeline import *
+
+if __name__ == '__main__':
+    gene = 'M'
+    getGene(gene, 'ATG[GT]CAGATT(.*)TGTACAGTAA')
+    alignSequences(gene)
+    createBoostrap(gene)
+    createDistanceMatrix(gene)
+    createUnrootedTree(gene)
+    createConsensusTree(gene)
