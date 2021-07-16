6eeb070b64410f479943958b7e0917685cbc82bf
my-linh-luu <mylinh_93@hotmail.com>
2021-07-15 22:20:19 -0400

    ajout de la fonction pour que la fenetre coulissante

--- pipeline.py
+++ pipeline.py
@@ -14,48 +14,168 @@ def menuGetTrees():
             print("The number cannot be lower than 1.")
         else:
             # VALIDER LE FORMAT NEWICK??
-            for i in range(int(count)):
-                try:
+            try:
+                for i in range(int(count)):
                     name = input("Name of the tree file (" + str(i+1) + "): " )
-                    f = open(name, "r")
+                    open(name, "r")
                     names.append(name)
-                except:
+            except:
                     print("This file does not exist or is empty.")
+                    raise Exception
             break
     return names
 
 
-# def getBootstrapThreshold():
+def getBootstrapThreshold():
+    valide = False
+    while not valide:
+        threshold = input("Enter the bootstrap value threshold between 0 and 100%: ")
+        valide = threshold.isnumeric() and int(threshold) < 100 and int(threshold) > 0
+        if not valide:
+            print("Error, it must be a number between 1 and 100.")
+        else:
+            return threshold
 
+
+def getRfThreshold():
+    valide = False
+    while not valide:
+        threshold = input("Enter the Robinson and Foulds distance threshold between 0 and 100%: ")
+        valide = threshold.isnumeric() and int(threshold) < 100 and int(threshold) > 0
+        if not valide:
+            print("Error, it must be a number between 1 and 100.")
+        else:
+            return threshold
+
+
+def getSlidingWindowSize():
+    while True:
+        size = input("Sliding window size: ")
+        try:
+            size = int(size)
+        except Exception:
+            print("The window size must be a number.")
+            continue
+        if(size <= 0):
+            print("The window size must be between greater than 0.")
+        else:
+            break
+    return size
+
+def getStepSize():
+    while True:
+        count = input("Step count: ")
+        try:
+            count = int(count)
+        except Exception:
+            print("The step count must be a number.")
+            continue
+        if(count <= 0):
+            print("The window size must be between greater than 0.")
+        else:
+            break
+
+
+def sliding_window(window_size=0, step=0):
+    # Permet d'avoir le nombre de lignes totales dans le fichier
+    f = open("infile", "r")
+    line_count = -1
+    for line in f:
+        if line != "\n":
+            line_count += 1
+    f.close()
+    f = open("infile", "r").read()
+    # premier nombre de la premiere ligne du fichier represente le nbr de sequences
+    num_seq = int((f.split("\n")[0]).split(" ")[0])
+    # second nombre de la premiere ligne du fichier represente la longueur des sequences
+    longueur = int((f.split("\n")[0]).split(" ")[1])
+    # permet d'obtenir le nbr de lignes qui compose chaque sequence
+    no_line = int(line_count/num_seq)
+
+    # Recupere la sequence pour chaque variante
+    with open("outfile", "w") as out:
+        depart = 1
+        fin = depart + no_line
+        # on connait la longueur de chaque sequence, donc on va recuperer chaque sequence et le retranscrire sur un autre fichier separes par un \n entre chaque
+        for i in range(0, int(num_seq)):
+            f = open("infile", "r")
+            lines_to_read = range(depart, fin)
+            for position, line in enumerate(f):
+                if position in lines_to_read:
+                    out.write(line)
+            out.write("\n")
+            depart = fin
+            fin = depart + no_line
+    out.close()
+
+    # on cree un fichier out qui contient chaque sequence sans espaces et on enregistre dans une list le nom en ordre des sequences
+    with open("outfile", "r") as out, open("out", "w") as f:
+        sequences = out.read().split("\n\n")
+        list_names = []
+        for seq in sequences:
+            s = seq.replace("\n", " ").split(" ")
+            if s[0] != "":
+                list_names.append(s[0])
+            s_line = s[1:len(seq)]
+            for line in s_line:
+                if line != "":
+                    f.write(line)
+            f.write("\n")
+    out.close()
+    f.close()
+
+    # slide the window along the sequence
+    debut = 0
+    fin = debut + window_size
+    while fin <= longueur:
+        index = 0
+        with open("out", "r") as f, open("output/windows/" + str(debut), "w") as out:
+            out.write(str(num_seq) + " " + str(window_size) + "\n")
+            for line in f:
+                if line != "\n":
+                    espece = list_names[index]
+                    nbr_espaces = 11 - len(espece)
+                    out.write(espece)
+                    for i in range(nbr_espaces):
+                        out.write(" ")
+                    out.write(line[debut:fin] + "\n")
+                    index = index + 1
+        out.close()
+        f.close()
+        debut = debut + step
+        fin = fin + step
+
+
+def printOptionMenu():
+    print('===============================================')
+    print('Please select an option among the following: ')
+    print('===============================================')
+    print('1. Use the whole DNA sequences')
+    print('2. Study specific genes of SARS-CoV-2')
+
+def validateOptionMenu(window_size, step_size):
+    while option != '1' or option != '2':
+        option = input("Please enter 1 or 2: ")
+        if option == '1':
+            reference.getReferenceTree()
+            break
+        elif option == '2':
+            print("ok2")
+            break
+        else:
+            print('This is not a valid option.')
 
 
 def menu(option=0):
-    names = menuGetTrees()
-    # print('===============================================')
-    # print('Please select an option among the following: ')
-    # print('===============================================')
-    # print('1. Use the whole DNA sequences')
-    # print('2. Study specific genes of SARS-CoV-2')
-    
-    # while option != '1' or option != '2':
-    #     option = input("Please enter 1 or 2: ")
-    #     if option == '1':
-    #         # faire un loop ici au cas ou il rentre nimporte quoi
-    #         option = input("Use a sliding window?[y/n]\n")
-    #         while True:
-    #             if option == 'Y' or option =='y': # ici l'utilisateur choisit la fenetre coulissante
-    #                 size = input('Window size:\n')
-    #                 if int(size) > 0:
-    #                     print('Yes')
-    #                     break
-    #             elif option == 'n' or option == 'N': # ici, pas de fenetre coulissante on peut analyser toutes les sequences
-    #                 reference.getReferenceTree()
-    #                 break
-    #     elif option == '2':
-    #         print("ok2")
-    #         break
-    #     else: 
-    #         print('This is not a valid option.')
+    try:
+        names = menuGetTrees()
+        bootstrap_threshold = getBootstrapThreshold()
+        rf_threshold = getRfThreshold()
+        window_size = getSlidingWindowSize()
+        step_size = getStepSize()
+        printOptionMenu()
+    except:
+        return
 
 
 def useWholeSequence(option=0):
