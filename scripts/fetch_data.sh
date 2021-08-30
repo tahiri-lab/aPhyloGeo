7636f0cdfa4d6c22a505a0335f55fb34d7b79b89
my-linh-luu <mylinh_93@hotmail.com>
2021-08-30 15:32:29 -0400

    commande pour fetch_data modifiee

--- scripts/fetch_data.sh
+++ scripts/fetch_data.sh
@@ -12,5 +12,5 @@ if [ -f $filename ]; then
 fi
 
 # Create a new sequences.fasta file containing all the sequences in the data repo
-find data/sequences -name "*.fasta" -exec cat {} >> output/reference_gene.fasta \;
+find data -name "*.fasta" -exec cat {} >> output/reference_gene.fasta \;
 
