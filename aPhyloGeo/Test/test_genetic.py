ef39c3b5d7bc1589a090f526f7570f42a01cea27
cetmus <143204687+cetmus@users.noreply.github.com>
2023-08-29 12:08:36 -0400

    Update test_genetic.py

--- aPhyloGeo/Test/test_genetic.py
+++ aPhyloGeo/Test/test_genetic.py
@@ -31,7 +31,7 @@ class TestGenetic():
         params_very_small = Params(os.path.join(os.path.dirname(__file__), "params_very_small.yaml"))
         sequences_very_small = aPhyloGeo.openFastaFile(params_very_small.reference_gene_file)
         very_small = AlignSequences(sequences_very_small, params_very_small.window_size, params_very_small.step_size,
-                                    params_very_small.makeDebugFiles, params_very_small.bootstrapAmount, params_very_small.alignment_method)
+                                    params_very_small.makeDebugFiles, params_very_small.bootstrapAmount, params_very_small.alignment_method, params_very_small.reference_gene_file)
 
         self.alignementSetup = [very_small]  # , small]
         self.paramSetup = [params_very_small]  # , params_small]
