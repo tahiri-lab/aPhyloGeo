55a7eba3ce65d3d20f096cfeb1542a571f899704
Nadia Tahiri <nadia.tahiri@umontreal.ca>
2025-07-01 20:40:20 -0400

    package name typo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -42,7 +42,7 @@ html_static_path = ['_static']
 html_context = {
     'display_github': True,
     'github_user': 'tahiri-lab',
-    'github_repo': 'aPhiloGeo',
+    'github_repo': 'aPhyloGeo',
     'github_version': 'main/',
     'conf_py_path': 'docs/',
     'source_suffix': '.rst',
