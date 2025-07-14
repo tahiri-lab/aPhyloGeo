9329055f05f2eee786020d0cd3f7c5a760e5d7e3
Ana Laura Chenoweth Galaz <analaura@Anas-MacBook-Air.local>
2025-07-14 09:53:24 -0400

    Mantel test in Cumacea dataset

--- scripts/generate_cumacea_output.py
+++ scripts/generate_cumacea_output.py
@@ -5,6 +5,8 @@ from aphylogeo.params import Params
 from Bio import Phylo
 from io import StringIO
 from pathlib import Path
+from aphylogeo.utils import get_patristic_distance_matrix
+from scipy.spatial.distance import pdist, squareform
 
 # Load parameters
 Params.load_from_file("tests/cumacea_align.yaml")
@@ -30,6 +32,25 @@ genetic_trees = {
 # Generate climatic trees from the dataframe using your defined pipeline
 print(f"[INFO] Generating climatic trees from climatic data...")
 climatic_trees = utils.climaticPipeline(df)
+
+# === Mantel test (statistical correlation) ===
+try:
+    print(f"\n\nRunning Mantel test {Params.mantel_test_method} with {Params.permutations_mantel_test} permutations... ")
+
+    # Prepare climatic distance matrix
+    climatic_matrix = df.drop(columns=[Params.specimen])
+    climatic_dist = squareform(pdist(climatic_matrix, metric="euclidean"))
+
+    # Prepare genetic distance matrix (simple version from trees)
+    genetic_dist = get_patristic_distance_matrix(genetic_trees)
+
+    # Run Mantel test
+    r, p, n = utils.run_mantel_test(genetic_dist, climatic_dist, Params.permutations_mantel_test, Params.mantel_test_method)
+
+    print(f"Mantel test result: \n r = {r:.3f} Correlation coefficient \n p = {p:.4f} Significance level \n n = {n} Number of observations\n")
+except Exception as e:
+    print(f"Could not compute Mantel test: {e}")
+
 
 # Compare trees and filter best matches
 print(f"[INFO] Comparing and filtering phylogenetic trees...")
